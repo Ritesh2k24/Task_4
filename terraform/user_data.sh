d999c371889ec55a0e8c6c3168c754bff27f696d
Ritesh2k24 <162950856+Ritesh2k24@users.noreply.github.com>
2025-04-19 23:17:44 +0530

    Update user_data.sh
    updated the code

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-apt update -y
-apt install -y docker.io
-systemctl start docker
+apt-get update -y
+apt-get install -y docker.io awscli
+
+
 systemctl enable docker
+systemctl start docker
 
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 905418107991.dkr.ecr.us-east-1.amazonaws.com
+# Login to ECR
+$(aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 905418107991.dkr.ecr.us-east-1.amazonaws.com)
 
+# Pull and run Strapi Docker image
 docker pull 905418107991.dkr.ecr.us-east-1.amazonaws.com/strapi-app:latest
 docker run -d -p 1337:1337 905418107991.dkr.ecr.us-east-1.amazonaws.com/strapi-app:latest
+
