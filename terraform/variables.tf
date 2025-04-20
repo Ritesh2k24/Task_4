1aa3c9de951a606df65d46f605c247ff71fccde9
Ritesh2k24 <162950856+Ritesh2k24@users.noreply.github.com>
2025-04-20 20:31:42 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,3 +6,11 @@ variable "ami_id" {
   description = "The AMI ID for the EC2 instance"
   type        = string
 }
+
+variable "aws_access_key_id" {
+  type = string
+}
+
+variable "aws_secret_access_key" {
+  type = string
+}
